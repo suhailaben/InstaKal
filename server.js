e2497ba3dcac0dba8dbbc32fb1d9dd23362039b6
mesaykk <mesay.kabew@hotmail.com>
2019-07-07 14:24:32 -0700

    use posts

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const users = require('./routes/api/users')
+const posts = requite('./routes/api/post')
 
 // Bring in my bodyParser 
 const bodyParser = require('body-parser');
@@ -19,6 +20,6 @@ app.use(bodyParser.json());
 app .get('/', (req, res) => res.send('Hello'));
 
 app.use('/api/users', users);
-
+app.use('/api/posts', posts)
 const port = 5005
 app.listen(port, () => console.log(`server is running on port ${port}`));
