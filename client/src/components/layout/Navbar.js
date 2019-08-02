15d946bcc88a6cc09b55d76e5b692416dc92ad39
Hannah0124 <hannahjoo24@gmail.com>
2019-08-01 22:37:53 -0700

    authLinks fix

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -16,16 +16,26 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
-          <a href="" onClick={this.onLogoutClick.bind(this)} className="nav-link">
-            <img
-              className="rounded-circle"
-              src={user.avatar}
+          <Link className="nav-link" to="/feed">
+            Post Feed 
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/dashboard">
+            Dashboard 
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="" onClick={this.onLogoutClick.bind(this)} className="nav-link">
+            <img 
+              className="rounded-circle" 
+              src={user.avatar} 
               alt={user.name}
-              style={{ width: '25px', marginRight: '5px '}}
-              title="You must have a Gravatar connected to your email to display an image." 
-            />
+              style={{ width: '25px', marginRight: '5px' }} 
+              title="You must have a Gravatar connected to your email to display an image."
+            />{' '}
             Logout
-          </a>
+          </Link>
         </li>
       </ul>
     );
